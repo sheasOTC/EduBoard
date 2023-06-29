953d0069c61bf45deabb3be476b6663ccad6d3da
sheasOTC <sheas@stu.otc.school.nz>
2023-06-29 13:27:27 +1200

    Created landing page

--- login.py
+++ login.py
@@ -1,15 +1,21 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import sqlite3
+import os
+
+
 
 # Connects to the sqlite3 database file; this file is automatically encrypted.
 
 con = sqlite3.connect("logins.db")
 cur = con.cursor()
-print(cur.fetchall())
 
+
+cur.execute("CREATE TABLE IF NOT EXISTS logins(emails,passwords)")
 
 # Login Function -- Validates information retrived from entries in the root 
+
 def login():
     correctP = False
     correctU = False
@@ -20,15 +26,13 @@ def login():
         if password[0] == entryPass.get():
             correctP = True
     if correctP and correctU == True:
-        loginFrame.grid_forget()
-        buttonSignup.grid_forget()
+        run_landing
     else:
-        return "Wrong email or password. \nPlease try again or contact your administrator."
+        messagebox.showerror("EduBoard","Wrong email or password. \nPlease try again or contact your administrator.")
     
 # Creates accounts for EduBoard - To be configured into administrator settings.
 
 def create_account():
-    labelFail.grid_forget()
     dupEmail = False
     dupPass = False
     for user in cur.execute("SELECT emails FROM logins").fetchall():
@@ -38,7 +42,7 @@ def create_account():
         if password[0] == entryPass.get() :
             dupPass = True
     if dupEmail or dupPass:
-        labelFail.grid()
+        messagebox.showerror('EduBoard', 'An account with this email already exists. \nPlease try again or contact your administrator.')
     
     if entryUser.get() == "" or entryPass.get() == "":
         print(entryUser.get())
@@ -47,20 +51,19 @@ def create_account():
                         ('{entryUser.get()}', '{entryPass.get()}')
                         """)
         con.commit()
-        print(cur.fetchall())
 
-
-    
-
+def run_landing():
+    os.system('landing.py')
 
 # Defines the root window -- Configures root window
+
 root = Tk()
 root.rowconfigure(6,weight=1)
 root.columnconfigure(0,weight=1)
 root.minsize(width=600, height=400)
 root.maxsize(width=1200, height=800)
 root.configure(bg='#717171')
-root.title("EduBoard")
+root.title("EduBoard - Login")
 
 
 reveal = Image.open('eyegraphic.png')
@@ -71,13 +74,13 @@ temp_filename = "temp.gif"
 composed_image.save(temp_filename, format="GIF")
 tk_image = PhotoImage(file=temp_filename).subsample(8)
 
-
-
-
 eduboard = Label(root,text="EduBoard", font=("Comic Sans MS", 48),bg='#717171')
 eduboard.grid(column=0,row=0,sticky='n')
 
+
+
 # Creates loginframe -- Used to display widgets that are used for loging in to EduBoard
+
 loginFrame = LabelFrame(root,text="Login", font=("Coolvetica", 24),labelanchor='n',bg='#717171')
 loginFrame.grid(column=0,row=1,sticky='n')
 labelUser = Label(loginFrame,text="Email",justify='center',bg='#717171')
@@ -90,7 +93,7 @@ entryPass = Entry(loginFrame,show='*',justify='center')
 entryPass.grid(column=0,row=5)
 buttonLogin = Button(loginFrame,text="Login",command=login,bg='#717171')
 buttonLogin.grid(column=0,row=7)
-labelFail = Label(loginFrame, text=login,fg='red',bg='#717171')
+
 
 
 buttonSignup = Button(root, text='Sign up', command=create_account, bg='#717171')
