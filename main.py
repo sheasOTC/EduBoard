65160aa6f0aabba5ef35d449b59af6f0ee5f5aa6
sheasOTC <sheas@stu.otc.school.nz>
2023-07-20 14:01:44 +1200

    Removed secondary window for login

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import sqlite3
 
 
+
 con = sqlite3.connect("logins.db")
 cur = con.cursor()
 
@@ -17,7 +18,7 @@ class Login:
         master.configure(bg='#717171')
         master.title("EduBoard - Login")
 
-        eduboard = Label(self.master,text="EduBoard", font=("Comic Sans MS", 48),bg='#717171')
+        eduboard = Label(self.master,text="EduBoard", font=("Comic Sans MS", 48),bg='#717171', )
         eduboard.grid(column=0,row=0,sticky='n')
 
         self.loginFrame = LabelFrame(self.master,text="Login", font=("Coolvetica", 24),labelanchor='n',bg='#717171')
@@ -32,14 +33,14 @@ class Login:
         self.entryPass.grid(column=0,row=5)
         self.buttonLogin = Button(self.loginFrame,text="Login",command=self.validate_login,bg='#717171')
         self.buttonLogin.grid(column=0,row=7)
-
         
-        buttonSignup = Button(self.master, text='Sign up', command=self.open_signup, bg='#717171')
-        buttonSignup.grid(column=0,row=2,pady=10)
+        self.buttonSignuppage = Button(self.master, text='Sign up', command=self.open_signup, bg='#717171')
+        self.buttonSignuppage.grid(column=0,row=2,pady=10)
 
     def validate_login(self):
         correctP = False
         correctU = False
+
         for user in cur.execute("SELECT emails FROM logins").fetchall():
             if user[0] == self.entryUser.get():
                 correctU = True
@@ -49,11 +50,19 @@ class Login:
         if correctP and correctU == True:
             self.master.quit()
         else:
-            messagebox.showerror("EduBoard","Wrong email or password. \nPlease try again or contact your administrator.")
+            if self.entryPass.get() == "" and self.entryUser.get() == "":
+                messagebox.showerror("EduBoard","Cannot leave password and email empty. \nPlease try again or contact your administrator.")
+            elif self.entryUser.get() == "":
+                messagebox.showerror("EduBoard","Cannot leave email empty. \nPlease try again or contact your administrator.")
+            elif self.entryPass.get() == "":
+                messagebox.showerror("EduBoard","Cannot leave password empty. \nPlease try again or contact your administrator.")
+            else:
+                messagebox.showerror("EduBoard","Wrong email or password. \nPlease try again or contact your administrator.")
 
     def open_signup(self):
-        self.signupWindow = Toplevel(self.master)
-        self.app = Signup(self.signupWindow)
+        self.loginFrame.destroy()
+        self.buttonSignuppage.destroy()
+        self.app = Signup(self.master)
         
 
 class Signup:
@@ -79,8 +88,10 @@ class Signup:
         self.labelPass.grid(column=0,row=4)
         self.entryPass = Entry(self.loginFrame,show='*',justify='center')
         self.entryPass.grid(column=0,row=5)
-        self.buttonLogin = Button(self.loginFrame,text="Sign Up",command=self.create_account,bg='#717171')
-        self.buttonLogin.grid(column=0,row=7)
+        self.buttonSignup = Button(self.loginFrame,text="Sign Up",command=self.create_account,bg='#717171')
+        self.buttonSignup.grid(column=0,row=6)
+
+        self.buttonLoginpage
 
     def create_account(self):
         dupEmail = False
@@ -93,9 +104,12 @@ class Signup:
                 dupPass = True
         if dupEmail or dupPass:
             messagebox.showerror('EduBoard', 'An account with this email already exists. \nPlease try again or contact your administrator.')
-        
-        if self.entryUser.get() == "" or self.entryUser.get() == "":
-            print(self.entryUser.get())
+        elif self.entryPass.get() == "" and self.entryUser.get() == "":
+                messagebox.showerror("EduBoard","Cannot leave password and email empty. \nPlease try again or contact your administrator.")
+        elif self.entryUser.get() == "":
+            messagebox.showerror("EduBoard","Cannot leave email empty. \nPlease try again or contact your administrator.")
+        elif self.entryPass.get() == "":
+            messagebox.showerror("EduBoard","Cannot leave password empty. \nPlease try again or contact your administrator.")
         else:
             cur.execute(f"""INSERT INTO logins VALUES
                             ('{self.entryUser.get()}', '{self.entryUser.get()}')
@@ -107,8 +121,9 @@ class Signup:
 
 def main(): 
     root = Tk()
-    app = Login(root)
+    Login(root)
     root.mainloop()
+    
 
 if __name__ == '__main__':
     main()
