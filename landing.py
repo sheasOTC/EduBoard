44680eb468c8537b734a6084197826e63a04b239
Sam <sheas@stu.otc.school.nz>
2023-07-21 10:28:29 +1200

    Created landing class

--- landing.py
+++ landing.py
@@ -1,17 +1,19 @@
 from tkinter import *
 
 
-landing = Tk()
-landing.rowconfigure(6,weight=1)
-landing.columnconfigure(0,weight=1)
-landing.minsize(width=600, height=400)
-landing.maxsize(width=1200, height=800)
-landing.configure(bg='#717171')
-landing.title("EduBoard - Main")
+self.master.rowconfigure(6,weight=1)
+self.master.columnconfigure(2,weight=1)
+self.master.minsize(width=600, height=400)
+self.master.maxsize(width=1200, height=800)
+self.master.configure(bg='#717171')
+self.master.title("EduBoard - Main")
 
 
-eduboard = Label(landing,text="EduBoard", font=("Comic Sans MS", 48),bg='#717171')
-eduboard.grid(column=0,row=0,sticky='n')
+labelEduboard = Label(self.master,text="EduBoard", font=("Comic Sans MS", 48),bg='#717171')
+labelEduboard.grid(column=0,row=0,sticky='n')
+
+eduboard = Label(self.master,text=f"{user}", font=("Comic Sans MS", 48),bg='#717171')
+eduboard.grid(column=2,row=0,sticky='n')
 
 
 
