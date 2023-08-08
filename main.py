b9440f62fac0216becc857281efbe48f0b2d7996
Sam <sheas@stu.otc.school.nz>
2023-08-09 09:55:51 +1200

    UI Redisgn

--- main.py
+++ main.py
@@ -7,35 +7,44 @@ import hashlib
 con = sqlite3.connect("logins.db")
 cur = con.cursor()
 
+try:
+    open("logins.db","x")
+except FileExistsError:
+    cur.execute("""CREATE TABLE IF NOT EXISTS
+                logins(emails, passwords)""")
+    con.commit()
+finally:
+    pass
+
 
 class Login:
     def __init__(self, master, offset = 2):
         self.master = master
         self.master.minsize(width=1200, height=800)
         self.master.maxsize(width=1200, height=800)
+        self.master.resizable(False, False)
         master.rowconfigure(6,weight=1)
         master.columnconfigure(2,weight=1)
-        master.configure(bg='#717171')
+        master.configure(bg='#0079b5')
         master.title("EduBoard - Login")
-        self.loggedin = False
 
-        self.eduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#717171', )
+        self.eduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#0079b5', )
         self.eduboard.grid(column=offset,row=2)
 
-        self.loginFrame = LabelFrame(self.master,text="Login", font=("Quicksand Bold", 36),labelanchor='n',bg='#717171',width=5,height=5)
+        self.loginFrame = LabelFrame(self.master,text="Login", font=("Quicksand Bold", 36),labelanchor='n',bg='#0079b5',width=5,height=5)
         self.loginFrame.grid(column=offset,row=3,sticky='n')
-        self.labelUser = Label(self.loginFrame,text="Email",justify='center',bg='#717171')
-        self.labelUser.grid(column=0,row=2)
-        self.entryUser = Entry(self.loginFrame,justify='center')
-        self.entryUser.grid(column=0,row=3)
-        self.labelPass = Label(self.loginFrame,text="Password",bg='#717171')
-        self.labelPass.grid(column=0,row=4)
-        self.entryPass = Entry(self.loginFrame,show='*',justify='center')
-        self.entryPass.grid(column=0,row=5)
-        self.buttonLogin = Button(self.loginFrame,text="Login",command=self.validate_login,bg='#717171')
-        self.buttonLogin.grid(column=0,row=7)
+        self.labelUser = Label(self.loginFrame,text="Email",justify='center',bg='#0079b5', font=("Quicksand Bold", 12))
+        self.labelUser.grid(column=3,row=2)
+        self.entryUser = Entry(self.loginFrame,justify='center',width=24)
+        self.entryUser.grid(column=3,row=3)
+        self.labelPass = Label(self.loginFrame,text="Password",bg='#0079b5', font=("Quicksand Bold",12))
+        self.labelPass.grid(column=3,row=4)
+        self.entryPass = Entry(self.loginFrame,show='*',justify='center',width=24)
+        self.entryPass.grid(column=3,row=5)
+        self.buttonLogin = Button(self.loginFrame,text="Login",command=self.validate_login,bg='#0079b5', font=("Quicksand Bold", 12), bd=0)
+        self.buttonLogin.grid(column=3,row=7)
         
-        self.buttonSignuppage = Button(self.master, text="Don't have an account? Sign up.", command=self.open_signup, bg='#717171',bd=0)
+        self.buttonSignuppage = Button(self.master, text="Don't have an account? Sign up.", command=self.open_signup, bg='#0079b5',bd=0, font=("Quicksand Bold",12))
         self.buttonSignuppage.grid(column=offset,row=4,pady=10)
 
     def validate_login(self):
@@ -57,7 +66,6 @@ class Login:
             self.eduboard.destroy()
             self.buttonSignuppage.destroy()
             self.loginFrame.destroy()
-            self.loggedin = True
             Landing(self.master,username)
         else:
             if self.entryPass.get() == "" and self.entryUser.get() == "":
@@ -80,26 +88,27 @@ class Signup:
         self.master = master
         self.master.minsize(width=1200, height=800)
         self.master.maxsize(width=1200, height=800)
-        master.configure(bg='#717171')
+        master.configure(bg='#0079b5')
         master.title("EduBoard - Signup")
 
-        self.eduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#717171')
+        self.eduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#0079b5')
         self.eduboard.grid(column=2,row=0,sticky='n')
     
-        self.loginFrame = LabelFrame(self.master,text="Sign Up", font=("Quicksand Bold", 24),labelanchor='n',bg='#717171')
+        self.loginFrame = LabelFrame(self.master,text="Sign Up", font=("Quicksand Bold", 36),labelanchor='n',bg='#0079b5')
         self.loginFrame.grid(column=2,row=1,sticky='n')
-        self.labelUser = Label(self.loginFrame,text="Email",justify='center',bg='#717171')
-        self.labelUser.grid(column=0,row=2)
-        self.entryUser = Entry(self.loginFrame,justify='center')
-        self.entryUser.grid(column=0,row=3)
-        self.labelPass = Label(self.loginFrame,text="Password",bg='#717171')
-        self.labelPass.grid(column=0,row=4)
-        self.entryPass = Entry(self.loginFrame,show='*',justify='center')
-        self.entryPass.grid(column=0,row=5)
-        self.buttonSignup = Button(self.loginFrame,text="Sign Up",command=self.create_account,bg='#717171')
-        self.buttonSignup.grid(column=0,row=6)
+        self.loginFrame.columnconfigure(2, weight=1)
+        self.labelUser = Label(self.loginFrame,text="Email",justify='center',bg='#0079b5', font=("Quicksand Bold", 12))
+        self.labelUser.grid(column=2,row=2)
+        self.entryUser = Entry(self.loginFrame,justify='center',width=24)
+        self.entryUser.grid(column=2,row=3)
+        self.labelPass = Label(self.loginFrame,text="Password",bg='#0079b5', font=("Quicksand Bold", 12))
+        self.labelPass.grid(column=2,row=4)
+        self.entryPass = Entry(self.loginFrame,show='*',justify='center',width=24)
+        self.entryPass.grid(column=2,row=5)
+        self.buttonSignup = Button(self.loginFrame,text="Sign Up",command=self.create_account,bg='#0079b5', font=("Quicksand Bold", 12),bd=0)
+        self.buttonSignup.grid(column=2,row=6)
 
-        self.buttonLoginpage = Button(self.master, text="Go Back", command=self.login_page,bd=0,bg='#717171')
+        self.buttonLoginpage = Button(self.master, text="Go Back", command=self.login_page,bd=0,bg='#0079b5', font=("Quicksand Bold", 12))
         self.buttonLoginpage.grid(row=2,pady=10,column=2)
 
     def create_account(self):
@@ -141,51 +150,45 @@ class Landing:
         self.master.minsize(width=1200, height=800)
         self.master.maxsize(width=1200, height=800)
         self.master.rowconfigure(6,weight=1)
-        self.master.columnconfigure(4,weight=1)
-        self.master.configure(bg='#717171')
+        self.master.columnconfigure(5,weight=1)
+        self.master.configure(bg='#0079b5')
         self.master.title("EduBoard - Main")
 
         
-        self.labelEduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#717171')
-        self.labelEduboard.grid(column=0,row=0,sticky='n')
+        self.labelEduboard = Label(self.master,text="EduBoard", font=("Quicksand Bold", 48),bg='#0079b5')
+        self.labelEduboard.grid()
 
-        self.username = Label(self.master,text=f"{user}", font=("Quicksand Bold", 38),bg='#717171')
-        self.username.grid(column=5,row=0,sticky='n',)
+        self.username = Label(self.master,text=f"{user}", font=("Quicksand Bold", 38),bg='#0079b5')
+        self.username.grid(column=6,row=0)
 
-        self.functionality = Frame(self.master, bg='#717171',bd=0)
+        self.functionality = Frame(self.master, bg='#0079b5',bd=0)
         self.functionality.grid(row=3,column=3)
 
         self.functionality.columnconfigure(0,weight=1)
         self.functionality.rowconfigure(3,weight=1)
 
-        self.buttonAttendance = Button(self.functionality, text="Attendance",width=17,height=1, font=("Quicksand Bold", 18),bg='#717171',pady=10)
-        self.buttonAttendance.grid(row=0,column=0)
+        self.buttonAttendance = Button(self.functionality, text="Attendance",width=15,height=1, font=("Quicksand Bold", 18),bg='#0079b5',pady=5)
+        self.buttonAttendance.grid(row=0,column=0,pady=10)
 
-        self.buttonLookup = Button(self.functionality, text="Lookup",width=15,height=1, font=("Quicksand Bold", 18),bg='#717171',pady=5)
-        self.buttonLookup.grid(row=1,column=0)
+        self.buttonLookup = Button(self.functionality, text="Lookup",width=15,height=1, font=("Quicksand Bold", 18),bg='#0079b5',pady=5)
+        self.buttonLookup.grid(row=1,column=0,pady=10)
 
-        self.buttonReports = Button(self.functionality, text="Reports",width=15,height=1, font=("Quicksand Bold", 18),bg='#717171',pady=5)
-        self.buttonReports.grid(row=2,column=0)
+        self.buttonReports = Button(self.functionality, text="Reports",width=15,height=1, font=("Quicksand Bold", 18),bg='#0079b5',pady=5)
+        self.buttonReports.grid(row=2,column=0,pady=10)
 
-        self.buttonSettings = Button(self.functionality, text='Settings',width=15,height=1, font=("Quicksand Bold", 18),bg='#717171',pady=5)
-        self.buttonSettings.grid(row=3,column=0)
+        self.buttonSettings = Button(self.functionality, text='Settings',width=15,height=1, font=("Quicksand Bold", 18),bg='#0079b5',pady=5)
+        self.buttonSettings.grid(row=3,column=0,pady=10)
 
-        self.buttonLogout = Button(self.master,text="Logout",command=self.login_page, font=("Quicksand Bold", 18),bg='#717171')
-        self.buttonLogout.grid(row=7,column=0,pady=50)
+        self.buttonLogout = Button(self.master,text="Logout",command=self.login_page, font=("Quicksand Bold", 18),bg='#0079b5')
+        self.buttonLogout.grid(row=7,column=0)
 
-        self.buttonHelp = Button(self.master,text="Help",command=self.help_menu, font=("Quicksand Bold", 18),bg='#717171')
-        self.buttonHelp.grid(row=7,column=4)
+        self.buttonHelp = Button(self.master,text="Help",command=self.help_menu, font=("Quicksand Bold", 18),bg='#0079b5')
+        self.buttonHelp.grid(row=7,column=6)
+    
     def login_page(self):
-        self.buttonLogout.destroy()
-        self.username.destroy()
-        self.labelEduboard.destroy()
-        self.functionality.destroy()    
-        self.buttonHelp.destroy()
-        self.master.columnconfigure(0,weight=1)
-        Login(self.master)
+        self.master.destroy()
+        main()
         
-
-
 
     def help_menu(self):
         help = Toplevel(self.master)
@@ -197,13 +200,6 @@ def main():
     Login(root)
     root.mainloop()
 
-try:
-    open("logins.db","x")
-    cur.execute("""CREATE TABLE 
-                logins(emails, passwords)""")
-    con.commit()
-except FileExistsError:
-    pass
 
 if __name__ == '__main__':
     main()
